39c9ba138b7b9d21d59e8e75338adf7dbd79e176
nvinod13 <nvinod13@gmail.com>
2024-05-16 13:08:44 +0530

    Update AIStudio.py

--- AIStudio.py
+++ AIStudio.py
@@ -106,13 +106,13 @@ def main():
         
         st.markdown(get_table_download_link(acquisition_data), unsafe_allow_html=True)
         def get_table_download_link(df):
-        """Generates a link allowing the data in a given panda dataframe to be downloaded
-        in:  dataframe
-        out: href string
-        """
-        csv = df.to_csv(index=False)
-        b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-        href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
+            """Generates a link allowing the data in a given panda dataframe to be downloaded
+            in:  dataframe
+            out: href string
+            """
+            csv = df.to_csv(index=False)
+            b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+            href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
 
         
         # st.markdown(f'<a href="data:file/csv;base64,{b64}" download="acquisition_data.csv">Download csv file</a>')
