623d691692524c5f0d35df27cae8a8c15163551f
nvinod13 <nvinod13@gmail.com>
2024-06-24 14:04:55 +0530

    Update RemittanceAgents.py

--- RemittanceAgents.py
+++ RemittanceAgents.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 def generate_sample_data():
     data = {
